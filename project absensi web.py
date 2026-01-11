04b99d1005fd87bf03f67be3a93d603a6e4495d9
benyroswandi <benyroswandi47@gmail.com>
2026-01-11 10:08:37 +0700

    Update project absensi web.py

--- project absensi web.py
+++ project absensi web.py
@@ -6,7 +6,7 @@ import time
 
 # --- KONFIGURASI GOOGLE SHEETS ---
 # GANTI LINK DI BAWAH INI DENGAN LINK GOOGLE SHEETS ABAH
-URL_SHEET = "https://https://docs.google.com/spreadsheets/d/1__d7A0qCxtkxnJT8oYXbmZfY1GAiFcyB600fBNQaJV8/edit?gid=0#gid=0"
+URL_SHEET = "https://docs.google.com/spreadsheets/d/1__d7A0qCxtkxnJT8oYXbmZfY1GAiFcyB600fBNQaJV8/export?format=csv"
 
 def main():
     st.set_page_config(page_title="SMK YPPT Absensi Online", layout="wide")
@@ -97,4 +97,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
